4e132e0cea12a082b2dc57647a3aec89b73aa8a3
Nir Gutman <46134640+NGBigField@users.noreply.github.com>
2019-12-11 00:37:44 +0200

    progression in Kalman Filter

--- Code/Kalman Filter - Matlab/Kalman_Filter.m
+++ Code/Kalman Filter - Matlab/Kalman_Filter.m
@@ -1,15 +1,14 @@
  classdef Kalman_Filter < handle
     
-    properties (SetAccess = private)
-        crnt_state = carState() ; %x
-        crnt_error  ;%P
+    properties
+        crntState = carState() ; %x
+        crntError  ;%P
         
-        n = 9 ; %dimentionality of system
+        n ;  % dimentionality of the system    /  number of values we estimate.
+        lambda  = 3.5  ;  % Scaling factor for the uncented version....  Currently fake and ugly.
+        
     end%private properties
-    
-    properties %public
-
-    end
+  
     
     %{   Private methods:
         ...  those should stay hidden from who-ever is using our kalman fiter.
@@ -18,15 +17,19 @@
 
     methods (Access = private)
         
-        function [predicted_state, predicted_error ]  = predict_state(    sensors_data , delta_t) 
-            %%
-            X = cell(1 , obj.n);  % sigma points
-            X{1} = obj.crnt_state;
-            for i = 1 : floor(obj.n / 2)
-                disp(i);
-            end
-            
-            
+        function [predicted_state, predicted_error ]  = predict_state(   delta_t) 
+       %%
+       %{ Building the Sigma-Points matrix:  }%
+       sigma_root = sqrtm(  obj.crntError  ); 
+     
+       positive_addition =  repmat( obj.crntState.vector , 1 , obj.n) + sqrt(obj.n + obj.lambda)*sigma_root ; 
+       negativ_addition =  repmat( obj.crntState.vector , 1 , obj.n) - sqrt(obj.n + obj.lambda)*sigma_root ; 
+       
+       X = [  obj.crntState.vector , positive_addition  ,  negativ_addition ]  ; %Sigma-Point matrix
+       
+       %creating mu_'
+       %creating sigma_'
+       
             
         end
         
@@ -42,8 +45,9 @@
         
         % c'tor  for a kalman filter.
         function obj = Kalman_Filter()
-            obj.crnt_state  = carState();
-            obj.crnt_error = zeros( obj.crnt_state.numValues ,  obj.crnt_state.numValues  )  ;
+            obj.crntState  = carState();
+            obj.crntError = zeros( obj.crntState.numValues ,  obj.crntState.numValues  )  ;
+            obj.n = obj.crntState.numValues;
         end
         
         
@@ -57,7 +61,7 @@
         end %update
         
         function state = get_currentState(obj)
-            state = obj.crnt_state ;
+            state = obj.crntState ;
         end
         
         
@@ -72,3 +76,6 @@
     
     
 end%class_def
+
+% %%
+%     obj.crntError  =    [   [3, 7,  0 , 0] ; [ 0, 0.2  ,  0 , 0] ; [0.1, 0,  0.05 , 0]  ; [0, 0,  0 , 0.87]    ] 
