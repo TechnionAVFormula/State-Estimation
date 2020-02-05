59b1c54ee7f7f52cee3f8b8f462f6704c206494f
orenelmakis <oren.elmakis@campus.technion.ac.il>
2020-02-05 17:49:38 +0200

    Sensors_Data

--- Kalmanfilter-python/ExcelReading.py
+++ Kalmanfilter-python/ExcelReading.py
@@ -4,7 +4,7 @@ import array
 
 
 def excel_function():
-    file_read = pd.read_excel("Sensors_Data.xlsx")
+    file_read = pd.read_csv("Sensors_Data.csv")
     file_read = pd.DataFrame(file_read).to_numpy()
     return file_read
 
