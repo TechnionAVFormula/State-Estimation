045c1357ad6f58404fd858d751e35d5a1ec851aa
orenelmakis <oren.elmakis@campus.technion.ac.il>
2020-02-05 18:01:32 +0200

    Fix

--- Kalmanfilter-python/ExcelReading.py
+++ Kalmanfilter-python/ExcelReading.py
@@ -4,7 +4,8 @@ import array
 
 
 def excel_function():
-    file_read = pd.read_csv("Sensors_Data.csv")
+    file_read = pd.read_csv(
+        r"C:\Users\orene\Documents\Formula\KalmanFilterPython2\State-Estimation\Kalmanfilter-python\Sensors_Data.csv"
+    )
     file_read = pd.DataFrame(file_read).to_numpy()
     return file_read
-
