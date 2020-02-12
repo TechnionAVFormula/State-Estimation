79c6b16c965c235784fdc28c3379efa0fc59b323
orenelmakis <oren.elmakis@campus.technion.ac.il>
2020-02-12 15:50:54 +0200

    Kalman_Update

--- Kalmanfilter-python/Kalman.py
+++ Kalmanfilter-python/Kalman.py
@@ -1,55 +1,68 @@
 import numpy as np
-import math
+from math import *
 from numpy.random import randn
-
 
 DTime = 0.001
 L_Tot = 1.535  # units [m], from Calculations,Weight Transfer file.
 L_Rear = 0.7675  # units [m],assuming the weight is distributed equal on the rear and front wheels need
-# need to be check.
-Gps_sig = 0.5  # the error of the GPS 0.5[m]
-angular_vel_sig = 0.01  # angular velocity error error 0.1[rad/sec]
-Steering_sig = 0.01  # The angle error for steering 0.01[rad]
 
-# Observation State relation matrix
-
-H = np.array([[1, 0, 0, 0, 0], [0, 1, 0, 0, 0], [0, 0, 0, 0, 0]])
-print(H)
-A = A = np.array(
-    [
-        [1, 0, DTime, 0, 0],
-        [0, 1, 0, DTime, 0],
-        [0, 0, 1, 0, 0],
-        [0, 0, 0, 1, 0],
-        [0, 0, 0, 0, 1],
-    ]
-)
 X_init = np.zeros([5, 1])
 P_init = 0.01 * np.eye(len(X_init))
 R = np.array([])
-print(P_init)
+B = np.array([[1], [2], [3], [4], [5]])
 
 
 def Prediction5d(X, u, Xd):
-    Steering = np.mod(math.atan2(Xd[1] - X[1], Xd[0] - X[0]) - X[4], 2 * math.pi)
-    Beta = np.mod(math.atan2(math.cos(Steering) * L_Rear, L_Tot), 2 * math.pi)
-    u[1] = (
-        np.linalg.norm(X[2:4]) * math.cos(Beta) * math.tan(Steering) / L_Tot
-    )  # Theta_dot
-    B = np.array(
+    Steering = np.mod(atan2(Xd[1] - X[1], Xd[0] - X[0]) - X[4], 2 * pi)
+    Beta = np.mod(atan2(cos(Steering) * L_Rear, L_Tot), 2 * pi)
+    V_tot = np.linalg.norm(X[2:4])
+    DTheta = V_tot * cos(Beta) * tan(Steering) / L_Tot
+    X_Prediction = np.array(
         [
-            [(DTime ** 2) / 2 * math.cos(X[4]), 0],
-            [(DTime ** 2) / 2 * math.sin(X[4]), 0],
-            [DTime * math.cos(X[4]), 0],
-            [DTime * math.sin(X[4]), 0],
-            [0, DTime],
-        ],
+            [X[0] + DTime * X[2] + (DTime ** 2) / 2 * cos(X[4] + Beta)],
+            [X[1] + DTime * X[3] + (DTime ** 2) / 2 * sin(X[3] + Beta)],
+            [X[2] + DTime * cos(X[4] + Beta)],
+            [X[3] + DTime * sin(X[4] + Beta)],
+            [X[4] + DTime * DTheta],
+        ]
     )
-    X_Prediction = (A @ X) + (B @ u)
-    return X_Prediction
+    return X_Prediction, Beta, Steering
 
 
+def Covariance_matrix(P, V, X, u, Beta, Steering):
+    J_x = np.array(
+        [
+            [1, 0, DTime, 0, -(DTime ** 2) * u[0] * sin(X[4] + Beta)],
+            [0, 1, 0, DTime, (DTime ** 2) * u[0] * cos(X[4] + Beta)],
+            [0, 0, 1, 0, -DTime * u[0] * sin(X[4] + Beta)],
+            [0, 0, 1, 0, DTime * u[0] * cos(X[4] + Beta)],
+            [0, 0, 0, 0, 1],
+        ]
+    )
+    J_v = np.array(
+        [
+            [(DTime ** 2) * cos(X[4] + Beta), 0],
+            [(DTime ** 2) * sin(X[4] + Beta), 0],
+            [DTime * cos(X[4] + Beta), 0],
+            [DTime * sin(X[4] + Beta), 0],
+            [
+                0,
+                DTime
+                * np.linalg.norm(X[2:4])
+                * cos(Beta)
+                / (L_Tot * (cos(Steering) ** 2)),
+            ],
+        ]
+    )
+    P_Pre = (J_x @ P @ np.transpose(J_x)) + (J_v @ V @ np.transpose(J_v))
+    return P_Pre
+
+
+P = 0.1 * np.eye(5)
+V = 0.01 * np.eye(2)
 X = np.array([[1], [2], [2], [2], [0]])
-B = u = np.array([[1], [0]])
+B = u = np.array([[10000], [0]])
 Xd = np.array([[11], [9], [5], [12]])
-print(Prediction5d(X, B, Xd))
+Xx, Beta, Steering = Prediction5d(X, B, Xd)
+P_Pre = Covariance_matrix(P, V, X, B, Beta, Steering)
+print(P_Pre)
--- Kalmanfilter-python/Sensors.py
+++ Kalmanfilter-python/Sensors.py
@@ -6,7 +6,9 @@ from ExcelReading import excel_function
 
 R = 6371 * pow(10, 3)  # Earth radius
 Eperimetertoangular = R * 2 * math.pi / 360  # Earth perimeter
-Tie_rod_wheel_pose = 46  # The longitude distance in [mm]
+Tie_rod_wheel_pose = (
+    70.6  # The longitude distance in [mm] the length 64[mm] and the height 30[mm]
+)
 Delta_time = 0.01  # Sampling time
 
 
@@ -36,7 +38,7 @@ def SteeringTranslate():
     arr = np.empty([len(Sensors_Data), 2])
     for i in range(0, len(Sensors_Data)):
         arr[i, :] = [
-            Delta_time * i / 60,
+            Delta_time * i,
             math.atan2(Sensors_Data[i, 2], Tie_rod_wheel_pose),
         ]
 
@@ -45,14 +47,17 @@ def SteeringTranslate():
 
 plt.figure(1)
 GPSpose = GPSTranslate()
-plt.plot(GPSpose[:, 0], GPSpose[:, 1])
+plt.plot(GPSpose[range(1, 1000), 0], GPSpose[range(1, 1000), 1])
 plt.grid()
+plt.axis("equal")
 
 plt.figure(2)
 Steering_angle = SteeringTranslate()
+# range(0, len(Steering_angle), 300)
+# range(0, len(Steering_angle), 300)
+print(range(1000))
 plt.plot(
-    Steering_angle[range(0, len(Steering_angle), 300), 0],
-    Steering_angle[range(0, len(Steering_angle), 300), 1] / math.pi * 90,
+    Steering_angle[range(1000), 0], Steering_angle[range(1000), 1] / math.pi * 180,
 )
 plt.grid()
 # print(Steering_angle[0:10, :])
