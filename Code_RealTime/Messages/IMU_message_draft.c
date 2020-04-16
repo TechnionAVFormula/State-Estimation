daf3dc8728351bda69398ed9eed868ef43e34604
Nir Gutman <46134640+NGBigField@users.noreply.github.com>
2020-04-16 20:31:09 +0300

    another version for  IMU_message_draft.c

--- Code_RealTime/Messages/IMU_message_draft.c
+++ Code_RealTime/Messages/IMU_message_draft.c
@@ -1,25 +1,22 @@
-
-
-
-
-message measureables
+// messages.sensors.IMUSensors
+message IMUSensors
 {
-    Vector3 linear_accelerion = 1
-    Vector3 linear_velocity = 10
-    Vector3 rotational_velocity = 2 
-    double steering_angle = 3   // radians. right positive
-    double wheel_velocity_rear_left = 4
-    double wheel_velocity_rear_right = 5
-    double wheel_velocity_front_left = 6
-    double wheel_velocity_front_right = 7
-    double throttle_position = 8
-    /*  */
-    double orientation = 9
+    Vector3D linear_accelerion = 1
+    Vector3D linear_velocity = 2
+    Vector3D rotational_velocity = 3 
+    double steering_angle = 4   // radians. right positive
+    double wheel_velocity_rear_left = 5
+    double wheel_velocity_rear_right = 6
+    double wheel_velocity_front_left = 7
+    double wheel_velocity_front_right = 8
+    double throttle_position = 9
+    double orientation = 10
 }
 
-
-message IMU
+// messages.sensors.GroundTruth 
+message GroundTruth
 {
-    measureables Measurments = 1
-    measureables Ground_Truth = 2
+    Vector3d                        Position = 1
+    messages.sensors.IMUSensors     IMU = 2
+    messages.perception.ConeMap     ConeMap = 3
 }
