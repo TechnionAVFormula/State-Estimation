b10e30964cb93dee56005ff3fd5a6fbaf59f8743
masterChiefer <yudagliz73@gmail.com>
2020-05-01 22:24:20 +0300

    worked on ordercones

--- Code_RealTime/OrderCones/orderConesMain.py
+++ Code_RealTime/OrderCones/orderConesMain.py
@@ -23,7 +23,7 @@ def orderCones(coneMap, carState  ):
 
 	#to order cones by delaunay
 	bluePoints , yellowPoints , blueLost ,yellowLost = orderByDeluanay(coneMap, carState)
-
+	#printMap(bluePoints , yellowPoints , blueLost ,yellowLost, carState)
 	return bluePoints  ,yellowPoints
 	
 def printMap(bluePoints , yellowPoints , blueLost ,yellowLost, carState):
@@ -32,24 +32,24 @@ def printMap(bluePoints , yellowPoints , blueLost ,yellowLost, carState):
 	my_canvas = tk.Canvas(root, width=1200, height=600)
 
 	for cone in bluePoints:
-		my_canvas.create_oval(cone.x-3,cone.y-3,cone.x+3,cone.y+3,fill="blue")
-		label = tk.Label(master = root,text=str(cone.order))
-		label.place(x=cone.x - 5,y= cone.y - 30)
+		my_canvas.create_oval(cone.position.x-3,cone.position.y-3,cone.position.x+3,cone.position.y+3,fill="blue")
+		#label = tk.Label(master = root,text=str(cone.order))
+		#label.place(x=cone.x - 5,y= cone.y - 30)
 	for cone in blueLost:
-		my_canvas.create_oval(cone.x-3,cone.y-3,cone.x+3,cone.y+3,fill="blue")
-		label = tk.Label(master = root,text=str(cone.order))
-		label.place(x=cone.x - 5,y= cone.y - 30)
+		my_canvas.create_oval(cone.position.x-3,cone.position.y-3,cone.position.x+3,cone.position.y+3,fill="blue")
+		#label = tk.Label(master = root,text=str(cone.order))
+		#label.place(x=cone.x - 5,y= cone.y - 30)
 	for cone in yellowPoints:
-		my_canvas.create_oval(cone.x-3,cone.y-3,cone.x+3,cone.y+3,fill="yellow")
-		label = tk.Label(master = root,text=str(cone.order))
-		label.place(x=cone.x - 5,y= cone.y - 30)
+		my_canvas.create_oval(cone.position.x-3,cone.position.y-3,cone.position.x+3,cone.position.y+3,fill="yellow")
+		#label = tk.Label(master = root,text=str(cone.order))
+		#label.place(x=cone.x - 5,y= cone.y - 30)
 	for cone in yellowLost:
-		my_canvas.create_oval(cone.x-3,cone.y-3,cone.x+3,cone.y+3,fill="yellow")
-		label = tk.Label(master = root,text=str(cone.order))
-		label.place(x=cone.x - 5,y= cone.y - 30)
+		my_canvas.create_oval(cone.position.x-3,cone.position.y-3,cone.position.x+3,cone.position.y+3,fill="yellow")
+		#label = tk.Label(master = root,text=str(cone.order))
+		#label.place(x=cone.x - 5,y= cone.y - 30)
 
-	my_canvas.create_oval(carState.x-3,carState.y-3,carState.x+3,carState.y+3,fill="purple")
-	my_canvas.create_line(carState.x,carState.y,carState.x+carState.Vx,carState.y+carState.Vy,fill="red")
+	my_canvas.create_oval(carState.position.x-3,carState.position.y-3,carState.position.x+3,carState.position.y+3,fill="purple")
+	#my_canvas.create_line(carState.x,carState.y,carState.x+carState.Vx,carState.y+carState.Vy,fill="red")
 	
 	my_canvas.pack()
 
