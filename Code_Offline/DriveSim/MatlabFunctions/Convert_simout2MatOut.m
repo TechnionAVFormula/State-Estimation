1211ccc8048c959d09fe25968ad2ff52a8a6c046
Alon <alonspinner@gmail.com>
2020-06-06 21:33:48 +0300

    Updated DriveSim
    *added 'start recording' functionatlity.
    press r to start recording. once record starts it will not stop.
    
    *added compass measurement (theta)

--- Code_Offline/DriveSim/MatlabFunctions/Convert_simout2MatOut.m
+++ Code_Offline/DriveSim/MatlabFunctions/Convert_simout2MatOut.m
@@ -13,6 +13,7 @@ MatOut.Measurements.IMU_Time=simout.Measurements.a_long.Time;
 MatOut.Measurements.a_long=simout.Measurements.a_long.Data;
 MatOut.Measurements.a_lat=simout.Measurements.a_lat.Data;
 MatOut.Measurements.omega=simout.Measurements.omega.Data;
+MatOut.Measurements.compass=simout.Measurements.compass.Data;
 MatOut.Measurements.delta=simout.Measurements.delta.Data;
 
 MatOut.Cones.Time=simout.Cones.LeftConesNoise.Time;
